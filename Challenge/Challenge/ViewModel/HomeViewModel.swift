1a68c1ec9505854c729a838a78be646efa5008c8
yeseul jang <yyssjj9999@gmail.com>
2026-03-16 09:38:54 +0900

    🚚build: VM 추가

---
+++ Challenge/Challenge/ViewModel/HomeViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewModel.swift
+//  Challenge
+//
+//  Created by Yeseul Jang on 3/12/26.
+//
+import Foundation
+import RxSwift
+import RxCocoa
+
+// 홈에 필요한 내용만 보낼 수 있도록
+// 접근제한자 적절히 쓰기
+
+final class HomeViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    func transform(input: Input) { // return Output??
+        
+    }
+}
