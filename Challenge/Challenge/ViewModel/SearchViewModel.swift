71191810418237c407fa57801312bf6a0aecf523
yeseul jang <yyssjj9999@gmail.com>
2026-03-17 17:09:06 +0900

    🚚build: SearchVM 추가

---
+++ Challenge/Challenge/ViewModel/SearchViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewModel.swift
+//  Challenge
+//
+//  Created by Yeseul Jang on 3/17/26.
+//
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class SearchViewModel {
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+    }
+    
+    struct Output {
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
