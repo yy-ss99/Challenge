add36a014f0d4e14dcc507b0d4d5d5b8da9aaf4f
yeseul jang <yyssjj9999@gmail.com>
2026-03-17 17:08:52 +0900

    🚚build: SearchViewController 파일 추가

---
+++ Challenge/Challenge/View/SearchView/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Challenge
+//
+//  Created by Yeseul Jang on 3/17/26.
+//
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+final class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
