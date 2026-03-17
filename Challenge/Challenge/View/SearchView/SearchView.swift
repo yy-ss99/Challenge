b02f08a4002564239cb288f543df0da673cde68f
yeseul jang <yyssjj9999@gmail.com>
2026-03-17 17:08:29 +0900

    🚚build: SearchViewController 추가

---
+++ Challenge/Challenge/View/SearchView/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  Challenge
+//
+//  Created by Yeseul Jang on 3/17/26.
+//
+import UIKit
+import RxSwift
+import SnapKit
+
+final class SearchView: UIView {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+    }
+}
